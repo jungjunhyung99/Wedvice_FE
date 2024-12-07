c1b981e23cc7df0243e2aeea4e02f6adc3ad7bcb
seonghyuk1 <tjdgur0737@naver.com>
2024-12-07 17:23:21 +0900

    [bugfix] PWA 관련 MetaData 재생성
    - 삭제된 MetaData 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
+import { Metadata } from 'next';
 import './globals.css';
 import BaseQueryClientProvider from '@/contexts/tanstackQuery/BaseQueryClientProvider';
+
+// PWA 관련 세팅
+export const metadata: Metadata = {
+  manifest: '/manifest.json',
+  title: 'Wedivce_FE 화이팅^^',
+  description: '테스트 메타 데이터 PWA',
+};
 
 export default function RootLayout({
   children,
