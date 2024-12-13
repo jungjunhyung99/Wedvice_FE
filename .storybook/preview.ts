8d4b64d1b8e475a88708dd31496ecdce75624da6
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-12-13 16:39:33 +0900

    [chore] Storybook SVGR 관련 세팅팅

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,6 +9,14 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      default: 'Dark', // 기본 테마 설정
+      values: [
+        // 선택 가능한 테마 설정
+        { name: 'Dark', value: '#17181C' },
+        { name: 'Light', value: '#FFFFFF' },
+      ],
+    },
   },
 };
 
