4c5c6d5710b157482640f30e31d646066dc87d27
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-12-13 16:39:04 +0900

    [chore] SVGR 설치 및 적용

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
+import { NextConfig } from 'next';
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
   // disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = withPWA({
+const path = require('path');
+
+const nextConfig: NextConfig = withPWA({
+  webpack: (config: any) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
   reactStrictMode: true,
 });
+
+module.exports = nextConfig;
