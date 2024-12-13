2c0210b6460d6343efe42ee1a019cd774d92a357
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-12-14 01:33:07 +0900

    [bugfix] font color 적용 안 되는 현상 수정

--- src/components/atoms/chip/Chip.tsx
+++ src/components/atoms/chip/Chip.tsx
@@ -4,17 +4,19 @@ import type { VariantProps } from 'class-variance-authority';
 import { cva } from 'class-variance-authority';
 
 const chipVariants = cva(
-  'flex flex-wrap w-full justify-center items-center text-white rounded-md font-[14px] h-[26px] px-[8px] py-[4px]',
+  'flex flex-wrap w-full justify-center items-center rounded-md text-[14px] h-[26px] px-[8px] py-[4px]',
   {
     variants: {
       type: {
-        wedding: 'bg-gray-200 rounded-md p-[8px] font-[16px] h-[38px]',
-        alarm: 'rounded-xl rounded-tl-none bg-gray-200 text-gray-900 h-[42px]',
-        primary300: 'bg-primary-300',
-        pink100: 'bg-pink-100',
-        teal100: 'bg-teal-100',
-        blue100: 'bg-blue-100',
-        yellow100: 'bg-yellow-100',
+        wedding:
+          'bg-gray-200 text-white rounded-md p-[8px] text-[16px] h-[38px]',
+        alarm:
+          'text-gray-900 rounded-xl rounded-tl-none bg-gray-200 text-gray-900 h-[42px]',
+        primary300: 'bg-primary-300 text-white',
+        pink100: 'bg-pink-100 text-white',
+        teal100: 'bg-teal-100 text-white',
+        blue100: 'bg-blue-100 text-white',
+        yellow100: 'bg-yellow-100 text-white',
         gray100: 'bg-gray-200 text-gray-800',
         red100: 'bg-red-100 text-red-200',
       },
