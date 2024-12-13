0b2b5c6a58498b26f7494fadde055531d90ae569
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-12-13 17:22:41 +0900

    [refactor] navigation을 위한 레이아웃 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from '@/components/atoms/button';
 import Image from 'next/image';
+import LeftArrow from '@/assets/left_arrow.svg';
 
 export default function Home() {
   return (
-    <div className="flex flex-col border-[3px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="flex flex-col items-center justify-center">
       <Image
         src="/icon512_rounded.png"
         alt="Next.js logo"
@@ -11,7 +12,12 @@ export default function Home() {
         height={38}
         priority
       />
-      <Button>simple button</Button>
+      <div className="w-[96px]">
+        <Button>
+          <LeftArrow />
+          <span>text</span>
+        </Button>
+      </div>
     </div>
   );
 }
