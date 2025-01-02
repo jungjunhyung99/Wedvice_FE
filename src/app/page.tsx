08cbbea8b51dd1fa030ab1935d234456c13cfbf1
seonghyuk1 <tjdgur0737@naver.com>
2025-01-03 01:16:10 +0900

    [feat] 카카오 Auth 관련 로그인
    - Login 버튼 생성 (이미지 포함 - 임시적으로 미디움 사이즈로 삽입)
    - 로그인 컴포넌트 및 Redirection될 페이지 생성
    - 백엔드 협업으로 카카오 Developer 관련 키 생성 : env 파일에 기입하였고
      보안상 올리지 않고 카톡으로 공유 드리겠습니다!
    - 백엔드 Redirect_uri 설정 이슈로 아직 테스트 불가합니다 승수님과
      연락하여 진행해보도록 하겠습니다!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Button } from '@/components/atoms/button';
 import Image from 'next/image';
 import LeftArrow from '@/assets/left_arrow.svg';
 
+import Login from '@/features/Login';
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
@@ -18,6 +20,8 @@ export default function Home() {
           <span>text</span>
         </Button>
       </div>
+      <Button>simple button</Button>
+      <Login />
     </div>
   );
 }
