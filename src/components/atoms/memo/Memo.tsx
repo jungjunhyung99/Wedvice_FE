a2da28a790885238b1309f9ca3f2f7dbfefeb0ad
vvizi <gmlwl70@gmail.com>
2025-01-19 19:58:50 +0900

    [feat] Memo atomic component 구현
    - MemoTextarea, MemoDiv 컴포넌트 구현
      - 텍스트 영역 크기 자동 조정 기능 추가 (너비와 높이 계산)
      - 텍스트 입력 시 최대 크기 제한 및 크기 계산 로직 구현
    
    - Memo 컴포넌트에 크기 및 편집 모드 관련 로직 추가
      - 크기별(minWidth, maxWidth, minHeight, maxHeight) 설정 값 정의
      - isEditMode에 따라 MemoTextarea 또는 MemoDiv 렌더링
    
    - 임시 span 요소를 사용한 텍스트 크기 계산 로직 구현
      - 동적 텍스트 너비 계산을 위한 createTempSpan 함수 추가
      - calculateNewHeight 함수로 텍스트 높이 계산
    
    - 스타일링 개선
      - 메모 크기별 디자인 반영 (small, medium)
      - placeholder 텍스트 및 기본 스타일 추가

--- src/components/atoms/memo/Memo.tsx
+++ src/components/atoms/memo/Memo.tsx
@@ -1,17 +1,21 @@
-import { cva, type VariantProps } from "class-variance-authority";
-import { forwardRef, type HTMLAttributes } from "react";
+"use client"
 
-const memoVariants = cva(
-  "inline-flex items-center py-2 px-3 rounded-md font-medium break-words border-[1px] border-white/20",
+import { forwardRef, useState } from "react";
+import MemoTextarea from "./MemoTextarea";
+import MemoDiv from "./MemoDiv";
+import { cva } from "class-variance-authority";
+
+export const memoVariants = cva(
+  "inline-flex items-center box-border text-center py-2 px-3 rounded-md leading-tight font-medium break-words border-[1px] border-white/20 bg-white/20 focus:outline-none focus:ring-0",
   {
     variants: {
       variant: {
-        main: "bg-white/20 text-white",
-        none: "text-gray-700",
+        main: "text-white",
+        none: "text-gray-700 placeholder-gray-700",
       },
       size: {
-        small: 'text-[14px] max-w-[152px] max-h-[52px]',
-        medium: 'text-[16px] max-w-[173px] max-h-[58px]',
+        small: 'text-[14px] max-w-[155px] max-h-[52px] min-w-[58px] min-h-[34px]',
+        medium: 'text-[16px] max-w-[173px] max-h-[58px] min-w-[63px] min-h-[37px]',
       },
     },
     defaultVariants: {
@@ -21,27 +25,55 @@ const memoVariants = cva(
   }
 );
 
-interface MemoProps
-  extends HTMLAttributes<HTMLDivElement>, VariantProps<typeof memoVariants> {}
+type SizeConfigItem = {
+  minWidth: number;
+  maxWidth: number;
+  minHeight: number;
+  maxHeight: number;
+};
+
+export type MemoSize = 'small' | 'medium';
+
+export const SIZE_CONFIG: Record<MemoSize, SizeConfigItem> = {
+  small: { minWidth: 58, maxWidth: 155, minHeight: 34, maxHeight: 52 },
+  medium: { minWidth: 63, maxWidth: 173, minHeight: 37, maxHeight: 58 },
+};
+
+interface MemoProps {
+  isEditMode?: boolean;
+  text?: string;
+  size?: MemoSize;
+}
 
 export const Memo = forwardRef<HTMLDivElement, MemoProps>(
-  ({ className, variant, size, children, ...props }, ref) => {
-    const isDefaultText = children === null;
-    const text = isDefaultText ? '메모...' : String(children).slice(0, 22);
+  ({ isEditMode = false, text = "", size = "medium", ...props }, ref) => {
+    const [memoText, setMemoText] = useState(text);
 
-    return (
-        <div
-            className={memoVariants({
-                variant: isDefaultText ? "none" : variant,
-                size,
-                className,
-            })}
-            ref={ref}
-            {...props}
-        >
-            {text}
-        </div>
+    const commonClass = memoVariants({
+      variant: memoText.trim() ? "main" : "none",
+      size,
+    });
+
+    return isEditMode ? (
+      <MemoTextarea
+        ref={ref as React.Ref<HTMLTextAreaElement>}
+        text={memoText}
+        size={size}
+        setMemoText={setMemoText}
+        className={commonClass}
+        {...props}
+        />
+    ) : (
+      <MemoDiv
+        ref={ref as React.Ref<HTMLDivElement>}
+        text={memoText}
+        size={size}
+        className={commonClass}
+        {...props}
+      />
     );
   }
 );
+
 Memo.displayName = "Memo";
+export default Memo;
