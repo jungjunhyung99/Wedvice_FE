9f4508f49e2e4620de7e81173488e54c1086c498
suji8073 <suzy8073@naver.com>
2025-01-27 20:25:49 +0900

    [chore] 날짜 선택 컴포넌트 구현 및 스타일 추가
    - CalendarSelect 컴포넌트: 날짜 선택 기능 구현
      - react-calendar 스타일링을 위해 styles.css 파일을 분리하여 관리
      - 좌우 네비게이션 버튼에 커스텀 SVG 아이콘 적용
      - 날짜 영역 선택 시 달력 fold-off 되며, 날짜 선택 시 fold-on 되도록 구현
    - styles.css: react-calendar 커스터마이징 스타일 관리
      - Tailwind CSS 대신 CSS 파일 분리 관리
      - 달력 요일 텍스트와 같은 특정 스타일은 CSS 클래스 접근이 더 효율적이라고 판단
    - index.tsx: CalendarSelect 컴포넌트 베럴 파일 추가

---
+++ src/components/molecules/calendarSelect/CalendarSelect.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { SetStateAction, useState } from 'react';
+import Calendar from 'react-calendar';
+import { format } from 'date-fns';
+import LeftArrow from '@/assets/left_arrow.svg';
+import RightArrow from '@/assets/right_arrow.svg';
+
+import './styles.css';
+
+export type DatePiece = Date | null;
+export type SelectedDate = DatePiece | [DatePiece, DatePiece];
+
+export const CalendarSelect = () => {
+  const [selectedDate, setSelectedDate] = useState<SelectedDate>();
+  const [isFolded, setIsFolded] = useState<boolean>(false);
+
+  const handleSelectedDate = (
+    newDate: SetStateAction<SelectedDate | undefined>,
+  ) => {
+    if (!(newDate instanceof Date)) return;
+
+    const isSameDate =
+      selectedDate instanceof Date &&
+      selectedDate.getTime() === newDate.getTime();
+
+    setSelectedDate(isSameDate ? undefined : newDate);
+    setIsFolded((prev) => !prev);
+  };
+
+  return (
+    <div className="rounded-lg bg-gray-100 p-5">
+      <div
+        className="flex cursor-pointer justify-between text-base font-medium"
+        onClick={() => setIsFolded(true)}
+      >
+        <span className="text-gray-800">날짜</span>
+        <span className={`${selectedDate ? 'text-white' : 'text-gray-500'}`}>
+          {selectedDate instanceof Date
+            ? format(selectedDate, 'yyyy년 M월 d일')
+            : '선택 안함'}
+        </span>
+      </div>
+
+      {isFolded && (
+        <Calendar
+          className="react-calendar"
+          onChange={handleSelectedDate}
+          value={selectedDate}
+          view="month"
+          calendarType="gregory"
+          prev2Label={null}
+          next2Label={null}
+          showNeighboringMonth={false}
+          formatDay={(_, date) => format(date, 'd')}
+          prevLabel={
+            <div className="mb-5 mr-1 flex h-6 w-6 items-center justify-center">
+              <LeftArrow />
+            </div>
+          }
+          nextLabel={
+            <div className="ml-1 flex h-6 w-6 items-center justify-center">
+              <RightArrow />
+            </div>
+          }
+          tileClassName={({ date }) =>
+            date < new Date() ? 'text-gray-400' : ''
+          }
+        />
+      )}
+    </div>
+  );
+};
---
+++ src/components/molecules/calendarSelect/index.ts
@@ -0,0 +1,1 @@
+export { CalendarSelect } from '@/components/molecules/calendarSelect/CalendarSelect';
