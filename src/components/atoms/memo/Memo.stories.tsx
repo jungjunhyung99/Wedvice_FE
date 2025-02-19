f2c8f398517f5fb1a7bbe6414d6204dd89964a6f
vvizi <gmlwl70@gmail.com>
2025-02-19 21:15:29 +0900

    [refactor] memo storybook 변경
    - MemoProvider 생성에 따른 적용

--- src/components/atoms/memo/Memo.stories.tsx
+++ src/components/atoms/memo/Memo.stories.tsx
@@ -1,9 +1,17 @@
+import { MemoProvider } from '@/contexts/memo/MemoProvider';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Memo } from './Memo';
 
 const meta: Meta<typeof Memo> = {
   title: 'Components/Memo',
   component: Memo,
+  decorators: [
+    (Story) => (
+      <MemoProvider>
+        <Story />
+      </MemoProvider>
+    ),
+  ],
   argTypes: {
     isEditMode: {
       control: 'boolean',
@@ -12,43 +20,15 @@ const meta: Meta<typeof Memo> = {
       control: { type: 'radio' },
       options: ['small', 'medium'],
     },
-    text: {
-      control: 'text',
-    },
   },
 };
 
 export default meta;
 type Story = StoryObj<typeof Memo>;
 
-export const SmallMemo: Story = {
+export const Default: Story = {
   args: {
     isEditMode: false,
     size: 'small',
-    text: '오늘도 화이팅!',
-  },
-};
-
-export const MediumMemo: Story = {
-  args: {
-    isEditMode: false,
-    size: 'medium',
-    text: '메모 테스트 메모 테스트 메모 테스트 메모 테스트 메모 테스트',
-  },
-};
-
-export const NoText: Story = {
-  args: {
-    isEditMode: false,
-    size: 'small',
-    text: '',
-  },
-};
-
-export const EditableMemo: Story = {
-  args: {
-    isEditMode: true,
-    size: 'medium',
-    text: '편집 모드',
   },
 };
