459c48ea0bd371a71539161d2f92bf0e68a050d5
vvizi <gmlwl70@gmail.com>
2025-02-27 23:14:20 +0900

    [feat] time 관련 유틸 함수 추가
    - yyyy.MM.dd 형식으로 날짜를 변환할 수 있는 유틸함수 추가

---
+++ src/utils/time.ts
@@ -0,0 +1,15 @@
+// 시간 및 날짜를 포맷팅하는 util 함수들을 정의합니다.
+
+/**
+ * time 문자열을 Date 객체로 생성한 뒤 yyyy.MM.dd 형식으로 변환
+ * 2025.02.27
+ */
+export const getFormattedTime = (time: string) => {
+  const timeDate = new Date(time);
+
+  const year = timeDate.getFullYear();
+  const month = `0${(timeDate.getMonth() + 1).toString()}`.slice(-2);
+  const date = `0${timeDate.getDate().toString()}`.slice(-2);
+
+  return `${year}.${month}.${date}`;
+};
