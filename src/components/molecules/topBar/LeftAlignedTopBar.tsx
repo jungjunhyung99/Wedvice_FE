3c81f2b0bb3d50ebd1e60a368dad1e7e577ab17c
vvizi <gmlwl70@gmail.com>
2025-02-28 00:13:37 +0900

    [feat] 타이틀 영역이 왼쪽 정렬된 TobBar 구현
    - title과 time 중 하나만 prop으로 받을 수 있음

---
+++ src/components/molecules/topBar/LeftAlignedTopBar.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { getFormattedYearMonth } from '@/utils/time';
+import { forwardRef, ReactNode } from 'react';
+
+interface BaseProps {
+  className?: string;
+  button?: ReactNode;
+  onButtonClick?: () => void;
+}
+
+// title과 time 중 하나만 받음
+type LeftAlignedTopBarProps =
+  | (BaseProps & { title: string; time?: never })
+  | (BaseProps & { time: string; title?: never });
+
+const LeftAlignedTopBar = forwardRef<HTMLDivElement, LeftAlignedTopBarProps>(
+  ({ className, title, time, button, onButtonClick, ...props }, ref) => {
+    const text = title || (time && getFormattedYearMonth(time));
+
+    return (
+      <div
+        ref={ref}
+        className={`${className} flex h-[53px] items-center px-5 text-center`}
+        {...props}
+      >
+        {/* 타이틀 || 타임 */}
+        {text && (
+          <span className='select-none text-xl font-semibold text-white'>
+            {text}
+          </span>
+        )}
+        {/* 우측 버튼 */}
+        {button && (
+          <div
+            className='ml-auto flex cursor-pointer items-center'
+            onClick={onButtonClick}
+          >
+            {button}
+          </div>
+        )}
+      </div>
+    );
+  },
+);
+
+LeftAlignedTopBar.displayName = 'LeftAlignedTopBar';
+export default LeftAlignedTopBar;
