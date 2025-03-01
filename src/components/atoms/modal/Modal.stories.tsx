dbd0fd8d1cd4fff8b340f43c64a1a90fa0dc2f6f
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2025-03-01 15:38:30 +0900

    [chore] addon-design 적용

--- src/components/atoms/modal/Modal.stories.tsx
+++ src/components/atoms/modal/Modal.stories.tsx
@@ -13,16 +13,16 @@ type Story = StoryObj<typeof Modal>;
 export const BasicModal: Story = {
   args: {
     children: (
-      <div className='flex flex-col justify-center items-center w-[320px]'>
-        <span className='text-white text-lg'>Title</span>
+      <div className='flex w-[320px] flex-col items-center justify-center'>
+        <span className='text-lg text-white'>Title</span>
 
-        <span className='text-gray-700 text-sm pt-3 pb-5'>Description</span>
+        <span className='pb-5 pt-3 text-sm text-gray-700'>Description</span>
 
         <div className='flex w-full justify-center gap-4'>
-          <button className='w-[130px] h-[52px] rounded-md bg-gray-300 text-gray-800'>
+          <button className='h-[52px] w-[130px] rounded-md bg-gray-300 text-gray-800'>
             Text
           </button>
-          <button className='w-[130px] h-[52px] rounded-md bg-red-200 text-white'>
+          <button className='h-[52px] w-[130px] rounded-md bg-red-200 text-white'>
             Text
           </button>
         </div>
@@ -31,20 +31,34 @@ export const BasicModal: Story = {
   },
 };
 
+BasicModal.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/cr2DuY0vceiMI5LlqWdKR2/Wedvice_%EB%94%94%EC%9E%90%EC%9D%B8?node-id=1403-38040&t=st8bJ1H7LQ9alnmh-4',
+  },
+};
+
 export const ConfirmModal: Story = {
   args: {
     children: (
-      <div className='flex flex-col justify-center w-[330px]'>
-        <span className='text-white font-bold text-lg'>Title</span>
+      <div className='flex w-[330px] flex-col justify-center'>
+        <span className='text-lg font-bold text-white'>Title</span>
 
-        <span className='text-gray-700 text-sm pt-3 pb-5'>Description</span>
+        <span className='pb-5 pt-3 text-sm text-gray-700'>Description</span>
 
         <div className='flex w-full justify-end gap-4'>
-          <button className='w-[56px] h-[34px] rounded-md bg-gray-300 text-gray-800'>
+          <button className='h-[34px] w-[56px] rounded-md bg-gray-300 text-gray-800'>
             Text
           </button>
         </div>
       </div>
     ),
+  },
+};
+
+ConfirmModal.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/design/cr2DuY0vceiMI5LlqWdKR2/Wedvice_%EB%94%94%EC%9E%90%EC%9D%B8?node-id=2294-24792&t=st8bJ1H7LQ9alnmh-4',
   },
 };
