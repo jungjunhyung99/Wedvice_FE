2dd323aea622ee6985391719a8388dbc16ae12a7
vvizi <gmlwl70@gmail.com>
2025-04-30 21:53:15 +0900

    [feat] Memo Layout 수정 및 BottomSheet 영역 추가
    - 기존 Link 기반 페이지 이동 방식 제거
    - 메모 영역 클릭 시 BottomSheet 열림
      - BottomSheet 내부에서 페이지 이동
      - 메모 초기화 버튼 추가
    - api 연동 후 리팩토링 필요

--- src/components/molecules/memo/MemoLink.tsx
+++ src/components/molecules/memo/MemoLink.tsx
@@ -1,23 +1,80 @@
-import Link from 'next/link';
+'use client';
 
+import HeartIcon from '@/assets/wed_icon/icon_40/icon_heart.svg';
+import { BottomSheet } from '@/components/atoms/bottomSheet';
+import { Button } from '@/components/atoms/button';
 import { Profile } from '@/components/atoms/profile/Profile';
+import useMemoContext from '@/contexts/memo/MemoContext';
 import { Memo } from '@/features/memo/component/Memo';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const BOTTOM_SHEET_HEIGHT = 224;
+type RoleType = '예신' | '예랑';
 
 export const MemoLink = () => {
-  const username = 'test'; // dummy data
+  const username = 'test';
+  const router = useRouter();
+  const { setMemoText } = useMemoContext();
+
+  const [open, setOpen] = useState(false);
+  const [selectedRole, setSelectedRole] = useState<RoleType | null>(null);
+
+  const handleNavigate = () => {
+    if (!selectedRole) {
+      return;
+    }
+    router.push(`/memo/${username}?role=${selectedRole}`);
+  };
+
+  const handleOpen = (role: RoleType) => {
+    setSelectedRole(role);
+    setOpen(true);
+  };
+
+  const MemoProfile = ({ role }: { role: RoleType }) => (
+    <div
+      onClick={() => handleOpen(role)}
+      className='flex cursor-pointer flex-col items-center -space-y-1'
+    >
+      <Profile size='medium' />
+      <Memo size='small' />
+    </div>
+  );
 
   return (
-    <div className='flex flex-row items-center justify-center'>
-      {/* TODO: 레이아웃 수정 필요 */}
-      {['예신', '예랑'].map((role) => (
-        <Link key={role} href={`/memo/${username}?role=${role}`}>
-          <div className='flex flex-col items-center -space-y-1'>
-            <Profile size='medium' />
-            <Memo size='small' />
-          </div>
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className='mt-14 flex items-center justify-center'>
+        <MemoProfile role='예신' />
+        <HeartIcon className='mx-2' />
+        <MemoProfile role='예랑' />
+      </div>
+      <BottomSheet
+        open={open}
+        onDismiss={() => setOpen(false)}
+        snapPoints={({ maxHeight }) => [BOTTOM_SHEET_HEIGHT, maxHeight]}
+        defaultSnap={() => BOTTOM_SHEET_HEIGHT}
+      >
+        <div className='mt-5 flex h-full flex-col items-center justify-center gap-5'>
+          <Button
+            onClick={handleNavigate}
+            variant={'primary_fill'}
+            size='lg'
+            width='register'
+          >
+            새 메모 남기기
+          </Button>
+          <Button
+            onClick={() => setMemoText('')}
+            variant={'gray_fill'}
+            size='lg'
+            width='register'
+          >
+            메모 삭제하기
+          </Button>
+        </div>
+      </BottomSheet>
+    </>
   );
 };
 
